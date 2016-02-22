3c0f259b012561ad553183c2421aff3e2bf1bef8
lembarek <lembarek2011@gmail.com>
2016-02-22 06:16:59 +0100

    firt init

---
+++ src/Providers/RoleServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lembarek\Role\Providers;
+
+use Lembarek\Core\Providers\ServiceProvider;
+
+class RoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->fullBoot('role',__DIR__.'/../');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
