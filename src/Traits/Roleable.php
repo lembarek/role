c0c9b6191732bfb149c4ca26c4c55175372dc7da
lembarek <lembarek2011@gmail.com>
2016-06-12 14:59:06 +0100

    add role functions to Roleable

--- src/Traits/Roleable.php
+++ src/Traits/Roleable.php
@@ -2,8 +2,12 @@
 
 namespace Lembarek\Role\Traits;
 
+use Lembarek\Auth\Models\User;
+use Lembarek\Role\Models\Role;
+
 trait Roleable
 {
+
     /**
     * return all roles for a user
     *
@@ -13,6 +17,7 @@ trait Roleable
     {
         return $this->belongsToMany('Lembarek\Role\Models\Role');
     }
+
 
     /**
     * check if the user has a role
@@ -30,6 +35,7 @@ trait Roleable
         return false;
     }
 
+
     /**
     * assign a role to a user
     *
@@ -40,4 +46,84 @@ trait Roleable
     {
         $this->roles()->attach($role);
     }
+
+
+    /**
+     * check if the current user is supurior then $user
+     *
+     * @param  User  $user
+     * @return boolean
+     */
+    public function isSuperiorThen($user)
+    {
+        if( ! $this->maxRole()) return false;
+        if( ! $user->maxRole()) return true;
+        return $this->maxRole()->order > $user->maxRole()->order;
+    }
+
+
+    /**
+     * return the most supurior role for a user
+     *
+     * @return Lembarek\Role\Models\Role
+     */
+    public function maxRole()
+    {
+        $roles = $this->roles();
+        $r = $roles->first();
+
+        foreach ($roles as $role){
+            if($role->order > $r->order)
+                $r = role;
+        }
+
+        return $r;
+    }
+
+
+    /**
+     * can add a role
+     *
+     * @param  Role  $role
+     * @return boolean
+     */
+    public function canAddRole(Role $role)
+    {
+        return $this->maxRole()->order >= $role->order;
+    }
+
+
+    /**
+     * can delete a role
+     *
+     * @param  User $user
+     * @return boolean
+     */
+    public function canDeleteRole(User $user)
+    {
+        return $this->maxRole()->order >= $user->maxRole()->order;
+    }
+
+
+    /**
+     * return all available role for this user
+     *
+     * @param User $user
+     * @return array
+     */
+    public function getRolesFor(User $user)
+    {
+
+        $userRoles = $user->roles()->get();
+
+        $roles  = Role::where('order', '<=', $this->maxRole()->order);
+
+        foreach ($userRoles as $role) {
+            $roles = $roles->where('name', '!=', $role->name);
+        }
+
+        return $roles->get();
+    }
+
+
 }
